8af1c8b3ef5760420691f54cdba53c30be74f67d
SarahKirsten <deranged.link@gmail.com>
2016-12-02 00:14:47 -0800

    All in source_code folder
    Created new project (notice says Timezones not TimeZones)
    Copy & Pasted all code from TimeZones to Timezones.
    Leaving TimeZones to be safe, in case of corruption

---
+++ source_code/Timezones/Timezones/City.h
@@ -0,0 +1,17 @@
+#pragma once
+#include<iostream>
+#include<fstream>
+#include<string>
+#include"Timezones.h"
+using namespace std;
+
+class City : public Timezones
+{
+private:
+	string city;
+	string citywish;
+	string attractions;
+public:
+	void getWish();
+	void getAttractions(string citywish);
+};
---
+++ source_code/Timezones/Timezones/Source.cpp
@@ -0,0 +1,40 @@
+#include "Header.h"
+#include "Timezones.h"//Including class for Timezones
+#include "City.h"//Including class for City
+
+
+//void fileinput();
+//void compare();
+//void kathmandu();
+//void kabul();
+//void calcutta();
+//void current();
+//void regular();
+
+int main()
+{
+	Timezones city;
+	/*city.setWish();*/
+	city.fileinput();
+	city.current();
+	city.compare();
+	if (city.wish == "Kathmandu")//If specific city is Kathmandu, because weird time zone
+	{
+		city.kathmandu();//calls function of kathmandu
+	}
+	else if (city.wish == "Kabul")//is kabul, because weird time zone
+	{
+		city.kabul();//calls function of kabul
+	}
+	else if (city.wish == "Calcutta" || city.wish == "NewDelhi" || city.wish == "Colombo")//weird timezone
+	{
+		city.calcutta();//calls function because weird timezone
+	}
+	else
+	{
+		city.regular();//calls function for regular timezones
+	}
+	return 0;
+}
+
+
